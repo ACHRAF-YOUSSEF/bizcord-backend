6e1da1e5df2cde2cd1f1abdefe04390b077ed321
ACHRAF YOUSSEF <yachraf403@gmail.com>
2026-02-09 14:50:29 +0100

    init

---
+++ src/test/java/com/bizcord/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizcord.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
